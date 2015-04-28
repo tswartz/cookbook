cd12807663b5b98be5b8e4b3931931c36fd04957
tswartz <tswartz92@gmail.com>
2015-04-27 23:57:06 -0400

    Add/edit dialog

--- public/pages/settings/settings.js
+++ public/pages/settings/settings.js
@@ -5,4 +5,10 @@ app.controller('SettingsCtrl', function($scope, $http, $location)
     $scope.selectCategory = function (category) {
         $scope.selectedCategory = category;
     };
+
+    $scope.addCategory = function () {
+        $scope.dialogTitle = "Add Category";
+        $scope.dialogAction = "Edit Category";
+        $('#addEditCategoryModal').modal('show');
+    }
 });
