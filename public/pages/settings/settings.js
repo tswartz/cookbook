ad6a077235a02158af341110419ae14843a4c7ef
tswartz <tswartz92@gmail.com>
2015-04-28 17:58:52 -0400

    Front end of editing category

--- public/pages/settings/settings.js
+++ public/pages/settings/settings.js
@@ -28,4 +28,32 @@ app.controller('SettingsCtrl', function($scope, $http, $location)
         }
         $('#addEditCategoryModal').modal('show');
     }
+
+    $scope.editCategory = function () {
+        if (!$scope.selectedCategory) {
+            $('#selectRecipeModal').modal('show');
+            return;
+        }
+        $scope.dialogTitle = "Edit Category";
+        $scope.dialogAction = "Edit";
+        $scope.newCategory = $scope.selectedCategory;
+        if ($scope.form) {
+            $scope.form.$setPristine();
+            $scope.form.$setUntouched();
+        }
+        $scope.submitCategory = function (newCategory) {
+            if ($scope.form.$invalid) return;
+            $('#addEditCategoryModal').modal('hide');
+            $http.put('/category', {newCategory: newCategory, oldCategory: $scope.selectedCategory})
+            .success(function (response) {
+                $scope.$parent.user = response;
+                $scope.selectCategory = newCategory;
+            })
+            .error(function (response) {
+                $scope.errorMessage = response;
+                $('#errorModal').modal('show');
+            });
+        }
+        $('#addEditCategoryModal').modal('show');
+    }
 });
