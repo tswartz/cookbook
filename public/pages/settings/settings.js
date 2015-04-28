465af0910919aabad2469a9e49bcb6993822bdec
tswartz <tswartz92@gmail.com>
2015-04-28 19:03:36 -0400

    Delete category dialog

--- public/pages/settings/settings.js
+++ public/pages/settings/settings.js
@@ -35,7 +35,7 @@ app.controller('SettingsCtrl', function($scope, $http, $location)
             return;
         }
         $scope.dialogTitle = "Edit Category";
-        $scope.dialogAction = "Edit";
+        $scope.dialogAction = "Save Changes";
         $scope.newCategory = $scope.selectedCategory;
         if ($scope.form) {
             $scope.form.$setPristine();
@@ -55,5 +55,30 @@ app.controller('SettingsCtrl', function($scope, $http, $location)
             });
         }
         $('#addEditCategoryModal').modal('show');
+    }
+
+    $scope.deleteCategory = function () {
+        if (!$scope.selectedCategory) {
+            $('#selectRecipeModal').modal('show');
+            return;
+        }
+        $scope.deleteCategory = {moveRecipes: true};
+        $scope.nonAllCategories = $scope.$parent.user.categories.filter(function(category){
+            return category != 'All' && category != $scope.selectedCategory;
+        });
+        if ($scope.form) {
+            $scope.form.$setPristine();
+            $scope.form.$setUntouched();
+        }
+        $scope.submitCategory = function (deleteCategory) {
+            if ($scope.form.$invalid) return;
+            var deleteData = {categoryToDelete : $scope.selectedCategory};
+            if (deleteCategory.moveRecipes) {
+                deleteData.newCategory = deleteCategory.newCategory;
+            }
+            
+            console.log(deleteCategory);
+        }
+        $('#deleteCategoryModal').modal('show');
     }
 });
